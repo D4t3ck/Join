237b7026d1d4eb6eb631247f8865d6373014fedd
Daniel Sinteck <d.sint3ck@gmail.com>
2024-02-02 09:20:25 +0100

    added login/signup toggle

--- js/login.js
+++ js/login.js
@@ -25,3 +25,29 @@ function toggleCheckboxSignup() {
 
   isImageSign = !isImageSign;
 }
+
+function toggleLogin() {
+  let login = document.getElementById("loginCard");
+  let signup = document.getElementById("signupCard");
+  let signupButton = document.getElementById("signUpContainer");
+
+  if (signup.style.display === "none" || signup.style.display === "") {
+    login.style.display = "none";
+    signupButton.style.display = "none";
+    signup.style.display = "flex";
+  } else {
+    login.style.display = "flex";
+    signupButton.style.display = "flex";
+    signup.style.display = "none";
+  }
+}
+
+function toggleSignupBack() {
+  let login = document.getElementById("loginCard");
+  let signup = document.getElementById("signupCard");
+  let signupButton = document.getElementById("signUpContainer");
+
+  login.style.display = "flex";
+  signupButton.style.display = "flex";
+  signup.style.display = "none";
+}
