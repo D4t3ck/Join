964d90efa76f3b986ad2dd096610531c12ebbc29
Roman Kinzele <romankinzel@outlook.dem>
2024-02-07 10:18:29 +0100

    update contacts

--- js/contacts.js
+++ js/contacts.js
@@ -24,75 +24,74 @@ let contacts = [
         "mail": "ClausCayer@test.de",
         "phone": "+49 1131 1111 1111"
     },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@test.de",
+        "phone": "+49 1111 1111 1111"
+    },
 ]
 
-// let letters = ['A','B','C','D','E','F','G','H','I','J','','','','','','','',];
-let letters = [];
+let letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+// let letters = [];
 
 function initContact() {
-    renderContacts();
     init();
     renderletters();
 }
 
-function renderContacts(filter) {
-    let contactArea = document.getElementById('contactScroll');
-    contactArea.innerHTML = '';
 
-    for (let i = 0; i < contacts.length; i++) {
-        const element = contacts[i];
-        const name = element['name'];
-        const mail = element['mail'];
-        const firstletter = element['name'].charAt(0);
-        console.log(firstletter)
-        if (!filter || filter == firstletter) {
-            
-        contactArea.innerHTML += 
-        
-        `
-        <div class="contacts_scroll_abc">
-        <div id="letterbox"></div>
-        </div>
-        <div class="contact_scrolls_stroke">
-        <img src="./assets/img/contacts/strokeGray.png" alt="" />
-          </div>
-        <div class="contact_scrolls_card_small">
-        <img
-          class="contact_scrolls_card_small_img"
-          src="./assets/img/contacts/ProfilebadgeAM.png"
-          alt=""
-        />
-        <div class="contact_scrolls_card_small_contact">
-          <span class="contact_scrolls_card_small_name">${name}</span>
-          <span class="contact_scrolls_card_small_onclick_email"
-            >${mail}</span
-          >
-        </div>
-      </div>`;
-
-        }
-
-        if (!letters.includes(firstletter)) {
-            letters.push(firstletter)
-        }
-    }
-}
-
-// function setFilter(letter) {
-//     renderContacts(letter)
-// }
 
 
 function renderletters() {
-    let letterbox = document.getElementById('letterbox')
-    letterbox.innerHTML = '';
-
+    let contactArea = document.getElementById('contactScroll');
+    contactArea.innerHTML = '';
+    // letterbox.innerHTML = '';
     for (let i = 0; i < letters.length; i++) {
         const letter = letters[i];
+  
+        for (let j = 0; j < contacts.length; j++) {
+            const element = contacts[j];
+            const name = element['name'];
+            const mail = element['mail'];
+            const firstletter = element['name'].charAt(0);
+            // console.log(firstletter)
+           
+            if (letter == firstletter) {
+                contactArea.innerHTML += 
+                /*html*/`              
+                <div class="contacts_scroll_abc">
+                   
+                        <div id="letterbox${i}"></div>
+                    
+                </div>
+                <div class="contact_scrolls_card_small">
+                    <img
+                    class="contact_scrolls_card_small_img"
+                    src="./assets/img/contacts/ProfilebadgeAM.png"
+                    alt=""
+                    />
+                    <div class="contact_scrolls_card_small_contact">
+                        <span class="contact_scrolls_card_small_name">${name}</span>
+                        <span class="contact_scrolls_card_small_onclick_email"
+                            >${mail}</span
+                        >
+                    </div>
+                </div>`; 
+            }
+        } 
+        let letterbox = document.getElementById(`letterbox${i}`)
+        if (letterbox) {
+            
+            letterbox.innerHTML += `
+            <span class="contacts_scroll_abc_text">${letter}</span>
+            <div class="contact_scrolls_stroke">
+            <img src="./assets/img/contacts/strokeGray.png" alt="" />
+        </div>
+    `;
+     
+        }
 
-        letterbox.innerHTML += `<span class="contacts_scroll_abc_text">${letter}</span>`;
-        // renderContacts(letter);
-        // setFilter(letter);
+
     }  
 }
 
@@ -105,6 +104,7 @@ function test() {
             "mail": email,
             "phone": number,
     }
-
-    console.log(contact)
+    
+    contacts.push(contact);
+    // console.log(contact)
 }
