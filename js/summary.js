8d07a57d2c1154a40c0f37d7e7bc469d31fffe8c
Daniel Sinteck <d.sint3ck@gmail.com>
2024-02-16 14:57:35 +0100

    last commit +

--- js/summary.js
+++ js/summary.js
@@ -30,17 +30,11 @@ async function initSummary() {
   let urgentNumber = document.getElementById("summary_urgent_number");
   urgentNumber.innerHTML = `${urgent.length}`;
 
-    console.log(tasks)
-
-
-   
-
-
-    getUserName();
-    getCurrentUser();
-// Rufe die Funktion `updateUpcomingDeadline` auf und übergebe das `tasks` Array.
-updateUpcomingDeadline(tasks);
-   
+  getUserName();
+  getCurrentUser();
+  greetingAnimation()
+  // Rufe die Funktion `updateUpcomingDeadline` auf und übergebe das `tasks` Array.
+  updateUpcomingDeadline(tasks);
 }
 
 /**
@@ -105,52 +99,51 @@ function updateUpcomingDeadline(tasks) {
  * @returns {boolean} - True if the category board is "toDo", otherwise false.
  */
 function checkTodos(todo) {
-    return todo.categoryBoard === "toDo";
-  }
-  
-  /**
-   * Checks if the todo's category board is "done".
-   * @param {Object} todo - The todo object to be checked.
-   * @returns {boolean} - True if the category board is "done", otherwise false.
-   */
-  function checkDone(todo) {
-    return todo.categoryBoard === "done";
-  }
-  
-  /**
-   * Checks if the todo's category board is "inProgress".
-   * @param {Object} todo - The todo object to be checked.
-   * @returns {boolean} - True if the category board is "inProgress", otherwise false.
-   */
-  function checkProgress(todo) {
-    return todo.categoryBoard === "inProgress";
-  }
-  
-  /**
-   * Checks if the todo's category board is "awaitFeedback".
-   * @param {Object} todo - The todo object to be checked.
-   * @returns {boolean} - True if the category board is "awaitFeedback", otherwise false.
-   */
-  function checkFeedback(todo) {
-    return todo.categoryBoard === "awaitFeedback";
-  }
-  
-  /**
-   * Checks if the todo's priority is "urgent".
-   * @param {Object} todo - The todo object to be checked.
-   * @returns {boolean} - True if the priority is "urgent", otherwise false.
-   */
-  function checkUrgent(todo) {
-    return todo.prio === "urgent";
-  }
-  
-  /**
-   * Changes the current page to the board page.
-   */
-  function goToBoard() {
-    changeCurrentPage("board");
-  }
-  
+  return todo.categoryBoard === "toDo";
+}
+
+/**
+ * Checks if the todo's category board is "done".
+ * @param {Object} todo - The todo object to be checked.
+ * @returns {boolean} - True if the category board is "done", otherwise false.
+ */
+function checkDone(todo) {
+  return todo.categoryBoard === "done";
+}
+
+/**
+ * Checks if the todo's category board is "inProgress".
+ * @param {Object} todo - The todo object to be checked.
+ * @returns {boolean} - True if the category board is "inProgress", otherwise false.
+ */
+function checkProgress(todo) {
+  return todo.categoryBoard === "inProgress";
+}
+
+/**
+ * Checks if the todo's category board is "awaitFeedback".
+ * @param {Object} todo - The todo object to be checked.
+ * @returns {boolean} - True if the category board is "awaitFeedback", otherwise false.
+ */
+function checkFeedback(todo) {
+  return todo.categoryBoard === "awaitFeedback";
+}
+
+/**
+ * Checks if the todo's priority is "urgent".
+ * @param {Object} todo - The todo object to be checked.
+ * @returns {boolean} - True if the priority is "urgent", otherwise false.
+ */
+function checkUrgent(todo) {
+  return todo.prio === "urgent";
+}
+
+/**
+ * Changes the current page to the board page.
+ */
+function goToBoard() {
+  changeCurrentPage("board");
+}
 
 /**
  * Updates the greeting message on the page based on the time of day and the username.
@@ -158,86 +151,86 @@ function checkTodos(todo) {
  * @param {string} userNameSelector - The selector of the element displaying the username.
  */
 async function updateGreetings(greetingTextSelector, userNameSelector) {
-    const response = await getItem("users");
-    const responseAsJson = JSON.parse(response);
-    const users = responseAsJson.users;
-  
-    const urlParams = new URLSearchParams(window.location.search);
-    const userMail = urlParams.get("mail");
-    const user = users.find((task) => task.userMail == userMail);
-  
-    const currentTime = new Date();
-    const currentHour = currentTime.getHours();
-  
-    const greetingText = document.getElementById(greetingTextSelector);
-    let greetingMessage = "";
-  
-    if (currentHour < 12) {
-      greetingMessage = "Good morning,";
-    } else if (currentHour < 18) {
-      greetingMessage = "Hello,";
-    } else {
-      greetingMessage = "Good evening,";
-    }
-  
-    greetingText.innerHTML = greetingMessage;
-  
-    if (userMail == null || userMail == "null") {
-      userName = "";
-  
-      const formatedGreeting = greetingText.innerText.replace(",", "");
-      greetingText.innerHTML = formatedGreeting;
-    } else {
-      const userName = user.userName;
-      const userNameDiv = document.getElementById(userNameSelector);
-      userNameDiv.innerHTML = `${userName}`;
-    }
+  const response = await getItem("users");
+  const responseAsJson = JSON.parse(response);
+  const users = responseAsJson.users;
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const userMail = urlParams.get("mail");
+  const user = users.find((task) => task.userMail == userMail);
+
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  const greetingText = document.getElementById(greetingTextSelector);
+  let greetingMessage = "";
+
+  if (currentHour < 12) {
+    greetingMessage = "Good morning,";
+  } else if (currentHour < 18) {
+    greetingMessage = "Hello,";
+  } else {
+    greetingMessage = "Good evening,";
   }
-  
-  /**
-   * Calls the updateGreetings function to display the username in the greeting message on the page.
-   */
-  async function getUserName() {
-    await updateGreetings("greeting", "user_name_greeting");
+
+  greetingText.innerHTML = greetingMessage;
+
+  if (userMail == null || userMail == "null") {
+    userName = "";
+
+    const formatedGreeting = greetingText.innerText.replace(",", "");
+    greetingText.innerHTML = formatedGreeting;
+  } else {
+    const userName = user.userName;
+    const userNameDiv = document.getElementById(userNameSelector);
+    userNameDiv.innerHTML = `${userName}`;
   }
-  
-  /**
-   * Updates the greeting message in the modal window based on the time of day and the username.
-   */
-  async function greetingAnimation() {
-    await updateGreetings("modal_greeting", "modal_user_name_greeting");
-  }
-  
-  /**
+}
+
+/**
+ * Calls the updateGreetings function to display the username in the greeting message on the page.
+ */
+async function getUserName() {
+  await updateGreetings("greeting", "user_name_greeting");
+}
+
+/**
+ * Updates the greeting message in the modal window based on the time of day and the username.
+ */
+async function greetingAnimation() {
+  await updateGreetings("modal_greeting", "modal_user_name_greeting");
+}
+
+/**
  * Executes when the DOM content is fully loaded. It initializes the animation modal based on certain conditions.
  */
 document.addEventListener("DOMContentLoaded", function () {
-    // Select the animation modal element
-    const animationModal = document.querySelector(".animation_modal");
-  
-    // Get the width of the screen
-    const screenWidth = window.innerWidth;
-  
-    // Get the referring page URL and convert it to lowercase
-    const referringPage = document.referrer.toLowerCase();
-    
-    // Check if the referring page is "index.html" and screen width is less than or equal to 1350px
-    if (referringPage.includes("index.html") && screenWidth <= 1350) {
-      // Add the "show" class to display the animation modal
-      animationModal.classList.add("show");
-  
-      // Set a timeout to remove the "show" class after 2 seconds
+  // Select the animation modal element
+  const animationModal = document.querySelector(".animation_modal");
+
+  // Get the width of the screen
+  const screenWidth = window.innerWidth;
+
+  // Get the referring page URL and convert it to lowercase
+  const referringPage = document.referrer.toLowerCase();
+
+  // Check if the referring page is "index.html" and screen width is less than or equal to 1350px
+  if (referringPage.includes("index.html") && screenWidth <= 1350) {
+    // Add the "show" class to display the animation modal
+    animationModal.classList.add("show");
+
+    // Set a timeout to remove the "show" class after 2 seconds
+    setTimeout(function () {
+      // Remove the "show" class to hide the animation modal
+      animationModal.classList.remove("show");
+
+      // Set another timeout to change the display property to "none" after the animation duration
       setTimeout(function () {
-        // Remove the "show" class to hide the animation modal
-        animationModal.classList.remove("show");
-  
-        // Set another timeout to change the display property to "none" after the animation duration
-        setTimeout(function () {
-          animationModal.style.display = "none";
-        }, 500); // Animation duration
-      }, 2000); // Delay before hiding the modal
-    } else {
-      // If conditions are not met, hide the animation modal
-      animationModal.style.display = "none";
-    }
-  });
+        animationModal.style.display = "none";
+      }, 500); // Animation duration
+    }, 2000); // Delay before hiding the modal
+  } else {
+    // If conditions are not met, hide the animation modal
+    animationModal.style.display = "none";
+  }
+});
